fcf3f752305bd12bb320ea971f0637a4f1e9a248
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-03-27 08:36:48 +0900

    Honoの環境構築

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,21 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+export interface Bindings  {
+  DB: D1Database;
+}
+const app = new Hono();
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
-})
+});
+
+// Todo取得API
+app.get('/api/todos', async c => {
+  if (!!c.env) {
+    const results = await (c.env.DB as D1Database).prepare('select * from todos').all();
+    const todos = results.results;
+    return c.json(todos);
+  }
+});
 
 export default app;
